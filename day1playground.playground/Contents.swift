40b05c5075d3e559616a80d2df694c393cc5f633
Apple <apple@Laptop-1-01.local>
2019-07-22 14:15:32 -0400

    changed, yo

---
+++ day1playground.playground/Contents.swift
@@ -0,0 +1,53 @@
+//import UIKit
+//
+//var hello = "Hello, playground"
+//hello
+//
+//
+//let name = "Leora"
+//let age = "16"
+//let first = "Karlie"
+//let last = "Kloss"
+//print ("I am \(age). My name is \(name).")
+//print ("\(first)\(last)")
+//print ("\(last)\(first)")
+//print ("\(first) \(last)")
+//print ("\(last) \(first) \(last) \(first)" )
+//print ("I love \(first) ")
+////
+//
+//var a = 12
+//var b = 65
+//var c = 31
+//var d = 98
+//var average = (Double(a + b + c + d) / 4)
+//
+//print (average)
+//
+//
+//name != first
+////
+//var dogAge = 3
+
+
+//if dogAge < 2 {
+//    print("you're a puppy lol")
+//}
+//else if dogAge > 12 {
+//    print("you are elderly")
+//}
+//else {
+//    print ("You are awesome")
+//}
+//
+
+var faveFood = "pizza"
+if faveFood == "pizza" {
+    print("Mmmm cheesy")
+}
+else if faveFood == "donuts" {
+    print("are you a krispy kreme or a dunkin donuts person?")
+}
+else {
+    print("incorrect")
+}
