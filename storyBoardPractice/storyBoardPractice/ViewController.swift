cab403bb60b84c11673e7a8f0402589d21be51af
Apple <apple@Laptop-1-01.local>
2019-07-25 12:01:39 -0400

    story board practice

---
+++ storyBoardPractice/storyBoardPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyBoardPractice
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 Leora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
